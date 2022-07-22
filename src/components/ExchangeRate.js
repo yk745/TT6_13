3a6b1e54e09bc061fe666dd2c1da94690f98022e
Dillon Yew <90141158+dillonwokie@users.noreply.github.com>
2022-07-22 16:11:15 +0800

    Add files via upload
    exchange rate  dashboard

---
+++ src/components/ExchangeRate.js
@@ -0,0 +1,50 @@
+import { useState, useCallback, useEffect } from "react";
+import { RateTable } from "./RateTable";
+import { CurrencyCodePicker } from "./CurrencyCodePicker";
+import { AmountField } from "./AmountField";
+import { getExchangeRates } from "../api";
+
+const supportedCurrencies = ["USD", "EUR", "JPY", "CAD", "GBP", "SGD"];
+
+export function ExchangeRate() {
+  const [amount, setAmount] = useState("1.00");
+  const [currencyCode, setCurrencyCode] = useState("SGD");
+  const [currencyData, setCurrencyData] = useState({ SGD: 1.0 });
+
+  useEffect(() => {
+    getExchangeRates(currencyCode, supportedCurrencies).then((rates) => {
+      setCurrencyData(rates);
+    });
+  }, [currencyCode]);
+
+  const handleCurrencyCode = useCallback(
+    (e) => setCurrencyCode(e.target.value),
+    []
+  );
+
+  const handleAmountChange = useCallback((e) => {
+    let newAmount = e.target.value;
+    setAmount(newAmount);
+  }, []);
+
+  return (
+    <>
+      <section>
+        <h1 className="ExchangeRate-header">
+          Exchange Rates{" "}
+          <CurrencyCodePicker
+            supportedCurrencies={supportedCurrencies}
+            currencyCode={currencyCode}
+            onChange={handleCurrencyCode}
+          />
+        </h1>
+      </section>
+      <section>
+        <AmountField amount={amount} onChange={handleAmountChange} />
+      </section>
+      <section>
+        <RateTable currencyData={currencyData} amount={amount} />
+      </section>
+    </>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ExchangeRate } from "./components/ExchangeRate";
+import "./style.css";
+
+ReactDOM.render(<ExchangeRate />, document.getElementById("root"));
+//
