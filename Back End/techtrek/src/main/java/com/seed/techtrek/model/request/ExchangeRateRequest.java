0afe23ef760c22b5a28323b2881b7ffb581643a4
Yong Khee <fooyongkhee@gmail.com>
2022-07-22 15:09:27 +0800

    1) refactored /countryExchangeRate api 2) added /home api

---
+++ Back End/techtrek/src/main/java/com/seed/techtrek/model/request/ExchangeRateRequest.java
@@ -0,0 +1,8 @@
+package com.seed.techtrek.model.request;
+
+import lombok.Data;
+
+@Data
+public class ExchangeRateRequest {
+    private String country;
+}
--- Back End/techtrek/src/main/java/com/seed/techtrek/repository/ExchangeRateRepository.java
+++ Back End/techtrek/src/main/java/com/seed/techtrek/repository/ExchangeRateRepository.java
@@ -2,10 +2,12 @@ package com.seed.techtrek.repository;
 
 import com.seed.techtrek.entity.ExchangeRate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Integer> {
 
-    ExchangeRate findByCountry(String country);
+    @Query()
+    ExchangeRate findByExchangeCurrency(String country);
 }
