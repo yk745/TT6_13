48083565164956e9a796e2108c4d34879da6b7d0
Yong Khee <fooyongkhee@gmail.com>
2022-07-22 11:28:57 +0800

    backend commit

---
+++ Back End/techtrek/src/test/java/com/seed/techtrek/TechtrekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seed.techtrek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechtrekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
