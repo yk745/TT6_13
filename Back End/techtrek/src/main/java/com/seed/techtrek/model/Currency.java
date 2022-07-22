0dbbb30c2f37a1dca3ec6e3c7f4ce0890c961e0d
getbuhcode <57144309+getbuhcode@users.noreply.github.com>
2022-07-22 12:34:07 +0800

    upload api, dao and model folder

---
+++ Back End/techtrek/src/main/java/com/seed/techtrek/model/Currency.java
@@ -0,0 +1,15 @@
+package com.seed.techtrek.model;
+
+public class Currency {
+    private final Int id;
+    private final Int wallet_id;
+    private final String currency = "";
+    private final Double amount = 0.00;
+
+    public Currency (Int id, Int wallet_id, String currency, Double amount){
+        this.id = id;
+        this.wallet_id = wallet_id;
+
+    }
+
+}
