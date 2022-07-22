049ce010130abf20d6a763f23119177244abfc38
BaoShinYeo <baoshin.yeo.2019@accountancy.smu.edu.sg>
2022-07-22 12:13:42 +0800

    added API controller

---
+++ Back End/techtrek/src/main/java/com/seed/techtrek/controller/APIController.java
@@ -0,0 +1,30 @@
+package com.seed.techtrek.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class APIController {
+
+
+    @RequestMapping(value="/login", method=RequestMethod.POST)
+    public User getCustomerIds(@RequestBody User user) {
+        return user;
+    }
+
+    @RequestMapping(value="/home", method=RequestMethod.POST)
+    public User getWallet(@RequestBody User user) {
+        return user;
+    }
+
+    @RequestMapping(value="/exchangeRate", method=RequestMethod.GET)
+    public ArrayList<Float> getExchangeRate() {
+        return new ArrayList<>();
+    }
+}
